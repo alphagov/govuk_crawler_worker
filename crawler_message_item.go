c213d3b77ed7c00b06da3dca31a62f7398c7de1c
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-05-12 16:23:33 +0100

    CrawlerMessageItem owns how to ExtractURLs from the response body

--- crawler_message_item.go
+++ crawler_message_item.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"log"
 	"net/http"
+	"net/url"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/streadway/amqp"
 )
 
@@ -17,4 +22,54 @@ func NewCrawlerMessageItem(delivery amqp.Delivery) *CrawlerMessageItem {
 
 func (c *CrawlerMessageItem) IsHTML() bool {
 	return http.DetectContentType(c.HTMLBody) == "text/html; charset=utf-8"
+}
+
+func (c *CrawlerMessageItem) ExtractURLs(host string) ([]string, error) {
+	urls := []string{}
+
+	document, err := goquery.NewDocumentFromReader(bytes.NewBuffer(c.HTMLBody))
+	if err != nil {
+		return urls, err
+	}
+
+	urlElementMatches := [][]string{
+		[]string{"a", "href"},
+		[]string{"img", "src"},
+		[]string{"link", "href"},
+		[]string{"script", "src"},
+	}
+
+	for _, attr := range urlElementMatches {
+		element, attr := attr[0], attr[1]
+		urls = append(urls, findByElementAttribute(document, host, element, attr)...)
+	}
+
+	return urls, err
+}
+
+func findByElementAttribute(document *goquery.Document, host string, element string, attr string) []string {
+	urls := []string{}
+
+	document.Find(element).Each(func(_ int, element *goquery.Selection) {
+		href, exists := element.Attr(attr)
+		unescapedHref, _ := url.QueryUnescape(href)
+		trimmedHref := strings.TrimSpace(unescapedHref)
+
+		u, err := url.Parse(trimmedHref)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if exists && len(trimmedHref) > 0 {
+			if u.Host == host {
+				urls = append(urls, trimmedHref)
+			}
+
+			if u.Host == "" && trimmedHref[0] == '/' {
+				urls = append(urls, "http://"+host+trimmedHref)
+			}
+		}
+	})
+
+	return urls
 }
--- crawler_message_item_test.go
+++ crawler_message_item_test.go
@@ -42,4 +42,92 @@ var _ = Describe("CrawlerMessageItem", func() {
 
 		Expect(item.IsHTML()).To(BeTrue())
 	})
+
+	Describe("ExtractURLs", func() {
+		var item *CrawlerMessageItem
+
+		BeforeEach(func() {
+			item = NewCrawlerMessageItem(delivery)
+		})
+
+		It("should return an empty array if it can't find any matching URLs", func() {
+			item.HTMLBody = []byte("")
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(Equal([]string{}))
+		})
+
+		It("should extract all a[@href] URLs from a given HTML document", func() {
+			item.HTMLBody = []byte(`<div><a href="https://www.foo.com/"></a></div>`)
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(ContainElement("https://www.foo.com/"))
+		})
+
+		It("should extract all img[@src] URLs from a given HTML document", func() {
+			item.HTMLBody = []byte(`<div><img src="https://www.foo.com/image.png" /></div>`)
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(ContainElement("https://www.foo.com/image.png"))
+		})
+
+		It("should extract all link[@href] URLs from a given HTML document", func() {
+			item.HTMLBody = []byte(`<head><link rel="icon" href="https://www.foo.com/favicon.ico"></head>`)
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(ContainElement("https://www.foo.com/favicon.ico"))
+		})
+
+		It("should extract all script[@src] URLs from a given HTML document", func() {
+			item.HTMLBody = []byte(
+				`<head><script type="text/javascript" src="https://www.foo.com/jq.js"></script></head>`)
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(ContainElement("https://www.foo.com/jq.js"))
+		})
+
+		It("successfully extracts multiple matching URLs from the provided DOM", func() {
+			item.HTMLBody = []byte(
+				`<head>
+<script type="text/javascript" src="https://www.foo.com/jq.js"></script>
+<link rel="icon" href="https://www.foo.com/favicon.ico">
+</head>`)
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(ContainElement("https://www.foo.com/jq.js"))
+			Expect(urls).To(ContainElement("https://www.foo.com/favicon.ico"))
+		})
+
+		It("will not provide URLs that don't match the provided prefix host", func() {
+			item.HTMLBody = []byte(
+				`<head><script type="text/javascript" src="https://www.foo.com/jq.js"></script></head>`)
+			urls, err := item.ExtractURLs("www.foobar.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(BeEmpty())
+		})
+
+		It("will unescape URLs", func() {
+			item.HTMLBody = []byte(`<div><a href="http://www.foo.com/bar%20"></a></div>`)
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(urls).To(ContainElement("http://www.foo.com/bar"))
+		})
+
+		It("should extract relative URLs", func() {
+			item.HTMLBody = []byte(`<div><a href="/foo/bar">a</a><a href="mailto:c@d.com">b</a></div>`)
+			urls, err := item.ExtractURLs("www.foo.com")
+
+			Expect(err).To(BeNil())
+			Expect(len(urls)).To(Equal(1))
+			Expect(urls).To(ContainElement("http://www.foo.com/foo/bar"))
+		})
+	})
 })
