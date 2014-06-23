337a81c0f95c780d341e950067673c4f65dc367e
Matt Bostock <matt@mattbostock.com>
2014-06-23 16:36:16 +0100

    Test WriteToDisk() and FilePath() methods
    Test the new WriteToDisk() and FilePath() methods on the
    CrawlerMessageItem interface.
    
    Move some of the fixture variables inside BeforeEach() so that we can
    re-use them across tests.
    
    We also now populate `item.HTMLBody` by default as the new FilePath()
    and WriteToDisk() methods will only act on content it detects as HTML.
    
    The test for WriteToDisk() will actually write to disk inside the
    directory specified by the MIRROR_ROOT environment variable, which must
    be set. I don't think this is an issue given that many of the other
    tests are also integration tests, i.e. they rely on networking to test
    crawling.
    
    Mocking in Go is also non-trivial. Testify's `mock`
    package[1] looks like a nice solution but I don't think it's necessary
    in this case.
    
    Also add a test for the URL() method on that interface.
    
    [1]: https://github.com/stretchr/testify#mock-package

--- crawler_message_item_test.go
+++ crawler_message_item_test.go
@@ -7,12 +7,34 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/streadway/amqp"
+	"os"
 )
 
 var _ = Describe("CrawlerMessageItem", func() {
-	host := "www.gov.uk"
-	example_url := "https://" + host + "/government/organisations"
-	delivery := amqp.Delivery{Body: []byte(example_url)}
+	var baseUrl, expectedFileName, host, mirrorRoot, testUrl, urlPath string
+	var delivery amqp.Delivery
+	var html []byte
+	var item *CrawlerMessageItem
+
+	BeforeEach(func() {
+		mirrorRoot = os.Getenv("MIRROR_ROOT")
+		host = "www.gov.uk"
+		baseUrl = "https://" + host
+
+		testUrl = baseUrl + urlPath
+		urlPath = "/government/organisations"
+		expectedFileName = mirrorRoot + urlPath + ".html"
+
+		delivery = amqp.Delivery{Body: []byte(testUrl)}
+		item = NewCrawlerMessageItem(delivery, host, []string{})
+
+		html = []byte(`<html>
+<head><title>test</title</head>
+<body><h1>TEST</h1></body>
+</html>
+`)
+		item.HTMLBody = html
+	})
 
 	It("generates a CrawlerMessageItem object", func() {
 		Expect(NewCrawlerMessageItem(delivery, host, []string{})).
@@ -21,12 +43,10 @@ var _ = Describe("CrawlerMessageItem", func() {
 
 	Describe("getting and setting the HTMLBody", func() {
 		It("can get the HTMLBody of the crawled URL", func() {
-			item := NewCrawlerMessageItem(delivery, host, []string{})
-			Expect(item.HTMLBody).To(BeNil())
+			Expect(item.HTMLBody).To(Equal(html))
 		})
 
 		It("can set the HTMLBody of the crawled URL", func() {
-			item := NewCrawlerMessageItem(delivery, host, []string{})
 			item.HTMLBody = []byte("foo")
 
 			Expect(item.HTMLBody).To(Equal([]byte("foo")))
@@ -34,15 +54,70 @@ var _ = Describe("CrawlerMessageItem", func() {
 	})
 
 	It("is able to state whether the content type is HTML", func() {
-		item := NewCrawlerMessageItem(delivery, host, []string{})
-		item.HTMLBody = []byte(`
-<html>
-<head><title>test</title</head>
-<body><h1>TEST</h1></body>
-</html>
-`)
-
 		Expect(item.IsHTML()).To(BeTrue())
+	})
+
+	It("returns its URL", func() {
+		item := NewCrawlerMessageItem(delivery, host, []string{})
+		Expect(item.URL()).To(Equal(testUrl))
+	})
+
+	Describe("writing crawled content to disk", func() {
+		It("wrote something to disk", func() {
+			fileName, _ := item.WriteToDisk()
+			Expect(fileName).To(Equal(expectedFileName))
+		})
+	})
+
+	Describe("generating a sane filename", func() {
+		It("strips out the domain, protocol, auth and ports", func() {
+			testUrl = "https://user:pass@example.com:8080/test/url"
+			expectedFileName = mirrorRoot + "/test/url"
+			delivery = amqp.Delivery{Body: []byte(testUrl)}
+			item = NewCrawlerMessageItem(delivery, host, []string{})
+
+			Expect(item.FileName()).To(Equal(expectedFileName))
+		})
+		It("strips illegal characters", func() {
+			testUrl = baseUrl + "/../!t@e£s$t/u^r*l(){}"
+			expectedFileName = mirrorRoot + "/test/url"
+			delivery = amqp.Delivery{Body: []byte(testUrl)}
+			item = NewCrawlerMessageItem(delivery, host, []string{})
+
+			Expect(item.FileName()).To(Equal(expectedFileName))
+		})
+		It("adds an index.html suffix when URL references a directory", func() {
+			testUrl = baseUrl + "/this/url/has/a/trailing/slash/"
+			expectedFileName = mirrorRoot + "/this/url/has/a/trailing/slash/index.html"
+			delivery = amqp.Delivery{Body: []byte(testUrl)}
+			item = NewCrawlerMessageItem(delivery, host, []string{})
+			item.HTMLBody = html
+
+			Expect(item.FileName()).To(Equal(expectedFileName))
+		})
+		It("adds an index.html suffix when URL has no path and no trailing slash", func() {
+			testUrl = baseUrl
+			expectedFileName = mirrorRoot + "/index.html"
+			delivery = amqp.Delivery{Body: []byte(testUrl)}
+			item = NewCrawlerMessageItem(delivery, host, []string{})
+			item.HTMLBody = html
+
+			Expect(item.FileName()).To(Equal(expectedFileName))
+		})
+		It("omits URL query parameters", func() {
+			delivery := amqp.Delivery{Body: []byte(testUrl + "?foo=bar")}
+			item = NewCrawlerMessageItem(delivery, host, []string{})
+			item.HTMLBody = html
+
+			Expect(item.FileName()).To(Equal(expectedFileName))
+		})
+		It("omits URL fragments", func() {
+			delivery := amqp.Delivery{Body: []byte(testUrl + "#foo")}
+			item = NewCrawlerMessageItem(delivery, host, []string{})
+			item.HTMLBody = html
+
+			Expect(item.FileName()).To(Equal(expectedFileName))
+		})
 	})
 
 	Describe("ExtractURLs", func() {
