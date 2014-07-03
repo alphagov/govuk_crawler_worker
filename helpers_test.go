5f86b81be807a05d2e9d6e372e76876e660789a6
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-07-03 15:57:01 +0100

    Move PurgeAllKeys func into helpers test file for reuse
    We're going to be purging all keys from Redis whenever we build up or
    use a Redis service (like ttl_hash_set objects). Share this code for
    easier reuse in `AfterEach` blocks.

--- helpers_test.go
+++ helpers_test.go
@@ -1,7 +1,11 @@
 package main_test
 
-import "log"
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/fzzy/radix/redis"
+)
 
 func DeleteMirrorFilesFromDisk(mirrorRoot string) {
 	if mirrorRoot != "" {
@@ -10,4 +14,23 @@ func DeleteMirrorFilesFromDisk(mirrorRoot string) {
 			log.Println(err)
 		}
 	}
+}
+
+func PurgeAllKeys(prefix string, address string) error {
+	client, err := redis.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+
+	keys, err := client.Cmd("KEYS", prefix+"*").List()
+	if err != nil || len(keys) <= 0 {
+		return err
+	}
+
+	reply := client.Cmd("DEL", keys)
+	if reply.Err != nil {
+		return reply.Err
+	}
+
+	return nil
 }
